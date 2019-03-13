64e4bb6a47172fafe359bb49736aba635174b3d3
liningning <liningning@wandoujia.com>
2019-03-13 12:03:52 +0800

    提交新的题目

---
+++ 208-实现Trie.py
@@ -0,0 +1,79 @@
+#!/bin/env python
+# -*-  coding: utf-8  -*-
+
+"""
+实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+
+示例:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // 返回 true
+trie.search("app");     // 返回 false
+trie.startsWith("app"); // 返回 true
+trie.insert("app");
+trie.search("app");     // 返回 true
+说明:
+
+你可以假设所有的输入都是由小写字母 a-z 构成的。
+保证所有输入均为非空字符串。
+
+"""
+
+
+class Trie(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = {}
+        self.end_of_word = "#"
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: None
+        """
+        node = self.root
+        for i in range(len(word)):
+            node = node.setdefault(word[i], {})
+        node[self.end_of_word] = {}
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for i in range(len(word)):
+            node = node.get(word[i], None)
+            if node is None:
+                return False
+        return self.end_of_word in node
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for i in range(len(prefix)):
+            node = node.get(prefix[i], None)
+            if node is None:
+                return False
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+obj = Trie()
+obj.insert("abcdef")
+print obj.search("abcdef")
+print obj.search("abcde")
+print obj.startsWith("abc")
+print obj.startsWith("ab1")
+print obj.startsWith("")
