84cd1a21fc53a9f605fb5d158e9b73c2909bd2f1
liningning <liningning@wandoujia.com>
2019-03-14 10:42:45 +0800

    提交新的题目

---
+++ 102-二叉树的层次遍历.py
@@ -0,0 +1,56 @@
+#!/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层次遍历结果：
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+"""
+
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+
+        queue = [root]
+        result = []
+
+        while queue:
+            level = []
+            for i in range(len(queue)):
+                node = queue.pop(0)
+                if node:
+                    level.append(node.val)
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+            result.append(level)
+
+        return result
