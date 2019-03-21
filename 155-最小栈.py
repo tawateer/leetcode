b62faffc16af37b75e9ba742665df704d34ce30d
liningning <liningning@wandoujia.com>
2019-03-21 15:29:44 +0800

    提交新的题目

---
+++ 155-最小栈.py
@@ -0,0 +1,70 @@
+#!/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+
+push(x) -- 将元素 x 推入栈中。
+pop() -- 删除栈顶的元素。
+top() -- 获取栈顶元素。
+getMin() -- 检索栈中的最小元素。
+示例:
+
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+"""
+
+
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack1.append(x)
+        if len(self.stack2) == 0:
+            self.stack2.append(x)
+        else:
+            if x <= self.stack2[-1]:
+                self.stack2.append(x)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        if self.stack1[-1] == self.stack2[-1]:
+            self.stack2.pop()
+        return self.stack1.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack1[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.stack2[-1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
