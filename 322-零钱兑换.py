acaf390a62a673c5c86b93a2e7da2469cabe706d
liningning <liningning@wandoujia.com>
2019-03-23 17:39:45 +0800

    提交新的题目

---
+++ 322-零钱兑换.py
@@ -0,0 +1,43 @@
+#!/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+
+示例 1:
+
+输入: coins = [1, 2, 5], amount = 11
+输出: 3
+解释: 11 = 5 + 5 + 1
+示例 2:
+
+输入: coins = [2], amount = 3
+输出: -1
+说明:
+你可以认为每种硬币的数量是无限的。
+"""
+
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        import sys
+
+        if amount == 0:
+            return 0
+        if len(coins) == 0:
+            return -1
+
+        dp = [sys.maxint] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            for j in range(len(coins)):
+                if i >= coins[j]:
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+
+        return dp[amount] if dp[amount] < sys.maxint else -1
