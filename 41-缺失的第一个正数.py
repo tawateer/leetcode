9f14dcae3940b14ba322583bb18a3bed5e262711
liningning <liningning@wandoujia.com>
2019-03-25 16:23:22 +0800

    提交新的题目

---
+++ 41-缺失的第一个正数.py
@@ -0,0 +1,46 @@
+#!/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+给定一个未排序的整数数组，找出其中没有出现的最小的正整数。
+
+示例 1:
+
+输入: [1,2,0]
+输出: 3
+示例 2:
+
+输入: [3,4,-1,1]
+输出: 2
+示例 3:
+
+输入: [7,8,9,11,12]
+输出: 1
+
+说明:
+你的算法的时间复杂度应为O(n)，并且只能使用常数级别的空间。
+"""
+
+
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i = 0
+        while i < len(nums):
+            if 0 < nums[i] <= len(nums) and nums[i] != nums[nums[i]-1]:
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+                i -= 1
+            i += 1
+
+        for i in range(len(nums)):
+            if nums[i] != i+1:
+                return i+1
+
+        return len(nums)+1
+
+
+s = Solution()
+print s.firstMissingPositive([1, 1])
