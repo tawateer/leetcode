4304d31f7e155214aecc2fdcab63ea5f9ef15be9
liningning <liningning@wandoujia.com>
2019-03-27 00:01:35 +0800

    提交新的题目

---
+++ 516-最长回文子序列.py
@@ -0,0 +1,46 @@
+#!/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+给定一个字符串s，找到其中最长的回文子序列。可以假设s的最大长度为1000。
+
+示例 1:
+输入:
+
+"bbbab"
+输出:
+
+4
+一个可能的最长回文子序列为 "bbbb"。
+
+示例 2:
+输入:
+
+"cbbd"
+输出:
+
+2
+一个可能的最长回文子序列为 "bb"。
+"""
+
+
+class Solution(object):
+    def longestPalindromeSubseq(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        length = len(s)
+
+        dp = [[0]*length for i in range(length)]
+        for i in range(length):
+            dp[i][i] = 1
+
+        for i in range(length-1, -1, -1):
+            for j in range(i+1, length):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i+1][j-1] + 2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+
+        return dp[0][length-1]
