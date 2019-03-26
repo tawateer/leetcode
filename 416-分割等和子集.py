afc8b50c15fb05a08a98bb0900c9902f868351ac
liningning <liningning@wandoujia.com>
2019-03-26 12:04:56 +0800

    提交新的题目

--- 416-分割等和子集.py
+++ 416-分割等和子集.py
@@ -63,8 +63,38 @@ class Solution(object):
                         dp[i][j] = 1
         return dp[-1][-1] == 1
 
+    def canPartition3(self, nums):
+        """
+        背包问题:
+            dp[i][j] 表示使用前 1...i 个元素组成不超过 j 的最大值.
+            dp[len(nums)][target] 如果等于 target 表示成功.
+        """
+        if len(nums) < 2:
+            return False
+
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+
+        target = total // 2
+        n = len(nums)
+
+        # 行是 nums, 列是 target, 长度都 +1
+        # 初始值: dp[0...n][0] = 0, 表示背包最大值是 0 的情况下最大放 0(废话)
+        dp = [[0]*(target+1) for _ in range(n+1)]
+        # i 对应 nums[i-1]
+        for i in range(1, n+1):
+            for j in range(1, target+1):
+                dp[i][j] = dp[i-1][j]   # 不选第 i 个元素
+                if j >= nums[i-1]:
+                    # 选第 i 个元素
+                    # i 对标 nums[i-1]
+                    dp[i][j] = max(dp[i][j], dp[i-1][j-nums[i-1]] + nums[i-1])
+
+        return dp[n][target] == target
+
 
 s = Solution()
-print s.canPartition2([1, 5, 11, 5])
-print s.canPartition2([1, 2, 3, 5])
-print s.canPartition2([1, 2, 5])
+print s.canPartition3([1, 5, 11, 5])
+print s.canPartition3([1, 2, 3, 5])
+print s.canPartition3([1, 2, 5])
