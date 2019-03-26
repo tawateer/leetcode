04d06621c999d51fa4c9cfe348f1a5b50ea86063
liningning <liningning@wandoujia.com>
2019-03-26 14:53:46 +0800

    add binarySearch.py

---
+++ 153-寻找旋转排序数组中的最小值.py
@@ -0,0 +1,46 @@
+#!/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+
+( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+
+请找出其中最小的元素。
+
+你可以假设数组中不存在重复元素。
+
+示例 1:
+
+输入: [3,4,5,1,2]
+输出: 1
+示例 2:
+
+输入: [4,5,6,7,0,1,2]
+输出: 0
+"""
+
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if not nums:
+            return -1
+
+        low, high = 0, len(nums) - 1
+        mid = 0
+        while low <= high:
+            mid = (low + high) / 2
+            if nums[low] <= nums[mid]:
+                if nums[mid] <= nums[high]:
+                    return nums[low]
+                else:
+                    low = mid + 1
+            else:
+                high = mid
+
+        return nums[mid]
