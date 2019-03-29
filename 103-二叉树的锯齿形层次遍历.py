635132490bf3dad96a0966f55a94e535a9923060
liningning <liningning@wandoujia.com>
2019-03-29 16:44:44 +0800

    提交新的题目

---
+++ 103-二叉树的锯齿形层次遍历.py
@@ -0,0 +1,57 @@
+#!/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+
+例如：
+给定二叉树 [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回锯齿形层次遍历如下：
+
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+
+        queue = [root]
+        result = []
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            print len(result), level
+            if len(result) & 1 == 0:
+                result.append(level)
+            else:
+                result.append(level[::-1])
+        return result
