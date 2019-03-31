d1902def939712baf2b56c5e7d50bd2a122aac4c
liningning <liningning@wandoujia.com>
2019-03-31 20:40:24 +0800

    提交新的题目

--- 215-数组中的第K个最大元素.py
+++ 215-数组中的第K个最大元素.py
@@ -24,21 +24,41 @@ import heapq
 
 class Solution(object):
     def findKthLargest(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        self.data = nums[0:k]
-        heapq.heapify(self.data)
+        data = nums[0:k]
+        heapq.heapify(data)
 
         for i in range(k, len(nums)):
-            if nums[i] >= self.data[0]:
-                heapq.heapreplace(self.data, nums[i])
-        return self.data[0]
+            if nums[i] >= data[0]:
+                heapq.heapreplace(data, nums[i])
+        return data[0]
+
+    def findKthLargest2(self, nums, k):
+        def partition(nums, p, r):
+            pivot = nums[r]
+            i, j = p, p
+            while j < r:
+                if nums[j] > pivot:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    i += 1
+                j += 1
+            nums[i], nums[r] = nums[r], nums[i]
+            return i
+
+        def findKthLargest(nums, p, r, k):
+            x = partition(nums, p, r)
+            if x + 1 == k:
+                return nums[x]
+            elif x + 1 > k:
+                return findKthLargest(nums, 0, x-1, k)
+            elif x + 1 < k:
+                return findKthLargest(nums, x+1, r, k)
+            else:
+                return -1
+
+        return findKthLargest(nums, 0, len(nums)-1, k)
 
 
 nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
 k = 4
 s = Solution()
-print s.findKthLargest(nums, k)
+print s.findKthLargest2(nums, k)
