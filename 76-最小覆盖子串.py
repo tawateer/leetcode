b403fe3c4a174b8a620d2c6e85681e18c1b63dbe
liningning <liningning@wandoujia.com>
2019-07-13 14:47:27 +0800

    提交新的题目

--- 76-最小覆盖子串.py
+++ 76-最小覆盖子串.py
@@ -16,48 +16,61 @@
 
 """
 
-
-def find(s, t):
-    res = ""
-    min_length = 99999
-    full_score = len(t)
-
-    meta_map = {}
-    for i in xrange(len(t)):
-        meta_map[t[i]] = meta_map.get(t[i], 0) + 1
-    # print meta_map
-
-    tmp_dict = {}
-    score = 0
-    i = 0
-    for j in range(len(s)):
-        if s[j] not in meta_map:
-            continue
-
-        tmp_dict[s[j]] = tmp_dict.get(s[j], 0) + 1
-        if tmp_dict[s[j]] <= meta_map[s[j]]:
-            score += 1
-
-        if score == full_score:
-            while i <= j:
-                if s[i] not in meta_map:
-                    i += 1
-                    continue
-
-                if tmp_dict[s[i]] == meta_map[s[i]]:
-                    if min_length > j - i + 1:
-                        min_length = j - i + 1
-                        res = s[i:i+min_length]
-
-                    tmp_dict[s[i]] = tmp_dict[s[i]] - 1
-                    score -= 1
-                    i += 1
-                    break
-
-                tmp_dict[s[i]] = tmp_dict[s[i]] - 1
-                i += 1
-
-    return res
+from collections import Counter
 
 
-print find("ADOBECODEBANC", "ABC")
+def minWindow(s, t):
+    if not t or not s:
+        return ""
+
+    # Dictionary which keeps a count of all the unique characters in t.
+    dict_t = Counter(t)
+
+    # Number of unique characters in t, which need to be present in the desired window.
+    required = len(dict_t)
+
+    # left and right pointer
+    l, r = 0, 0
+
+    # formed is used to keep track of how many unique characters in t are present in the current window in its desired frequency.
+    # e.g. if t is "AABC" then the window must have two A's, one B and one C. Thus formed would be = 3 when all these conditions are met.
+    formed = 0
+
+    # Dictionary which keeps a count of all the unique characters in the current window.
+    window_counts = {}
+
+    # ans tuple of the form (window length, left, right)
+    ans = float("inf"), None, None
+
+    while r < len(s):
+
+        # Add one character from the right to the window
+        character = s[r]
+        window_counts[character] = window_counts.get(character, 0) + 1
+
+        # If the frequency of the current character added equals to the desired count in t then increment the formed count by 1.
+        if character in dict_t and window_counts[character] == dict_t[character]:
+            formed += 1
+
+        # Try and co***act the window till the point where it ceases to be 'desirable'.
+        while l <= r and formed == required:
+            character = s[l]
+
+            # Save the smallest window until now.
+            if r - l + 1 < ans[0]:
+                ans = (r - l + 1, l, r)
+
+            # The character at the position pointed by the `left` pointer is no longer a part of the window.
+            window_counts[character] -= 1
+            if character in dict_t and window_counts[character] < dict_t[character]:
+                formed -= 1
+
+            # Move the left pointer ahead, this would help to look for a new window.
+            l += 1
+
+        # Keep expanding the window once we are done co***acting.
+        r += 1
+    return "" if ans[0] == float("inf") else s[ans[1] : ans[2] + 1]
+
+
+print minWindow("ADOBECODEBANC", "ABC")
