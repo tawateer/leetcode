b3e12890db7fce06c56a4278f020fc153bbb48a5
liningning <liningning@wandoujia.com>
2019-07-18 12:05:03 +0800

    add dijkstra.java

--- algo/dijkstra.java
+++ algo/dijkstra.java
@@ -1,9 +1,7 @@
 
-
 public class Graph { // 有向有权图的邻接表表示
   private LinkedList<Edge> adj[]; // 邻接表
   private int v; // 顶点个数
-
   public Graph(int v) {
     this.v = v;
     this.adj = new LinkedList[v];
@@ -11,7 +9,6 @@ public class Graph { // 有向有权图的邻接表表示
       this.adj[i] = new LinkedList<>();
     }
   }
-
   public void addEdge(int s, int t, int w) { // 添加一条边
     this.adj[s].add(new Edge(s, t, w));
 
@@ -26,7 +23,6 @@ public class Graph { // 有向有权图的邻接表表示
       this.w = w;
     }
   }
-
   // 下面这个类是为了 dijkstra 实现用的
   private class Vertex {
     public int id; // 顶点编号 ID
@@ -38,24 +34,20 @@ public class Graph { // 有向有权图的邻接表表示
   }
 }
 
-
 // 因为 Java 提供的优先级队列，没有暴露更新数据的接口，所以我们需要重新实现一个
 private class PriorityQueue { // 根据 vertex.dist 构建小顶堆
   private Vertex[] nodes;
   private int count;
-
   public PriorityQueue(int v) {
     this.nodes = new Vertex[v+1];
     this.count = v;
   }
-
   public Vertex poll() { // TODO: 留给读者实现... }
   public void add(Vertex vertex) { // TODO: 留给读者实现...}
   // 更新结点的值，并且从下往上堆化，重新符合堆的定义。时间复杂度 O(logn)。
   public void update(Vertex vertex) { // TODO: 留给读者实现...} 
   public boolean isEmpty() { // TODO: 留给读者实现...}
 }
-
 
 public void dijkstra(int s, int t) { // 从顶点 s 到顶点 t 的最短路径
   int[] predecessor = new int[this.v]; // 用来还原最短路径
@@ -76,11 +68,9 @@ public void dijkstra(int s, int t) { // 从顶点 s 到顶点 t 的最短路径
     for (int i = 0; i < adj[minVertex.id].size(); ++i) {
       Edge e = adj[minVertex.id].get(i); // 取出一条 minVetex 相连的边
       Vertex nextVertex = vertexes[e.tid]; // minVertex-->nextVertex
-
       if (minVertex.dist + e.w < nextVertex.dist) { // 更新 next 的 dist
         nextVertex.dist = minVertex.dist + e.w;
         predecessor[nextVertex.id] = minVertex.id;
-
         if (inqueue[nextVertex.id] == true) {
           queue.update(nextVertex); // 更新队列中的 dist 值
         } else {
