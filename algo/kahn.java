42a69f1956260235227a5eb2da657141bd514b0b
liningning <liningning@wandoujia.com>
2019-07-18 11:45:45 +0800

    add kahn.java

---
+++ algo/kahn.java
@@ -0,0 +1,46 @@
+
+public class Graph {
+  private int v; // 顶点的个数
+  private LinkedList<Integer> adj[]; // 邻接表
+
+  public Graph(int v) {
+    this.v = v;
+    adj = new LinkedList[v];
+    for (int i=0; i<v; ++i) {
+      adj[i] = new LinkedList<>();
+    }
+  }
+
+  public void addEdge(int s, int t) { // s 先于 t，边 s->t
+    adj[s].add(t);
+  }
+}
+
+
+// Kahn 算法
+public void topoSortByKahn() {
+  int[] inDegree = new int[v]; // 统计每个顶点的入度
+
+  for (int i = 0; i < v; ++i) {
+    for (int j = 0; j < adj[i].size(); ++j) {
+      int w = adj[i].get(j); // i->w
+      inDegree[w]++;
+    }
+  }
+
+  LinkedList<Integer> queue = new LinkedList<>();
+  for (int i = 0; i < v; ++i) {
+    if (inDegree[i] == 0) queue.add(i);
+  }
+
+  while (!queue.isEmpty()) {
+    int i = queue.remove();
+    System.out.print("->" + i);
+    for (int j = 0; j < adj[i].size(); ++j) {
+      int k = adj[i].get(j);
+      inDegree[k]--;
+      if (inDegree[k] == 0) queue.add(k);
+    }
+  }
+}
+
