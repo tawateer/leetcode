8f4925081847d454aa298252db488ff6d908f44d
liningning <liningning@wandoujia.com>
2019-08-03 20:44:08 +0800

    add floyd.java

---
+++ algo/floyd.java
@@ -0,0 +1,54 @@
+public class MatrixUDG {
+
+    private int mEdgNum;        // 边的数量
+    private char[] mVexs;       // 顶点集合
+    private int[][] mMatrix;    // 邻接矩阵
+    private static final int INF = Integer.MAX_VALUE;   // 最大值
+
+    ...
+}
+
+/*
+ * floyd最短路径。
+ * 即，统计图中各个顶点间的最短路径。
+ *
+ * 参数说明：
+ *     path -- 路径。path[i][j]=k表示，"顶点i"到"顶点j"的最短路径会经过顶点k。
+ *     dist -- 长度数组。即，dist[i][j]=sum表示，"顶点i"到"顶点j"的最短路径的长度是sum。
+ */
+public void floyd(int[][] path, int[][] dist) {
+
+    // 初始化
+    for (int i = 0; i < mVexs.length; i++) {
+        for (int j = 0; j < mVexs.length; j++) {
+            dist[i][j] = mMatrix[i][j];    // "顶点i"到"顶点j"的路径长度为"i到j的权值"。
+            path[i][j] = j;                // "顶点i"到"顶点j"的最短路径是经过顶点j。
+        }
+    }
+
+    // 计算最短路径
+    for (int k = 0; k < mVexs.length; k++) {
+        for (int i = 0; i < mVexs.length; i++) {
+            for (int j = 0; j < mVexs.length; j++) {
+
+                // 如果经过下标为k顶点路径比原两点间路径更短，则更新dist[i][j]和path[i][j]
+                int tmp = (dist[i][k]==INF || dist[k][j]==INF) ? INF : (dist[i][k] + dist[k][j]);
+                if (dist[i][j] > tmp) {
+                    // "i到j最短路径"对应的值设，为更小的一个(即经过k)
+                    dist[i][j] = tmp;
+                    // "i到j最短路径"对应的路径，经过k
+                    path[i][j] = path[i][k];
+                }
+            }
+        }
+    }
+
+    // 打印floyd最短路径的结果
+    System.out.printf("floyd: \n");
+    for (int i = 0; i < mVexs.length; i++) {
+        for (int j = 0; j < mVexs.length; j++)
+            System.out.printf("%2d  ", dist[i][j]);
+        System.out.printf("\n");
+    }
+}
+
